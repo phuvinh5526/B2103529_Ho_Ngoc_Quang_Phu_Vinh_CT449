d714e8125af68c946c6609c69b4fac9a5a090632
PhuVinh <phuvinh2003hg@gmail.com>
2025-02-26 16:12:20 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const ApiError = require("./app/api-error");
 
 const app = express();
 
@@ -13,5 +14,14 @@ app.get("/", (req, res)=>{
 });
 
 app.use("/api/contacts", contactsRouter);
+
+app.use((req, res, next) => {
+    return next(new ApiError (404, "Resource not found"));
+});
+app.use((err, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+    message: error.message || "Internal Server Error",
+    });
+});
 
 module.exports = app;
