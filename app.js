ddc55afcb0afa321ff7238118c84fd13602cb876
PhuVinh <phuvinh2003hg@gmail.com>
2025-02-26 15:49:19 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-// const contactsRouter = require("./app/routes/contact.route");
-// const ApiError = require("./app/api-error");
 
 const app = express();
+
+const contactsRouter = require("./app/routes/contact.route");
 
 app.use(cors());
 app.use(express.json());
@@ -11,16 +11,7 @@ app.use(express.json());
 app.get("/", (req, res)=>{
     res.json({message: "Welcome to contact book application. "});
 });
-// app.use("/api/contacts", contactsRouter);
-// 
-// app.use((req, res, next) =>{
-    // Khi khong co route nao duoc goi
-    // return next(new ApiError(404, "Resource not found"));
-// });
-// app.use((error, req, res, next)=>{
-    // return res.status(error.statusCode || 500).json({
-        // message: error.message || "Internal Server Error",
-    // });
-// });
+
+app.use("/api/contacts", contactsRouter);
 
 module.exports = app;
